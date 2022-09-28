f154eba2699d8fd786e5c89c4b8b7b1ab449f142
Anis-H100 <99392078+Anis-H100@users.noreply.github.com>
2022-09-28 19:22:23 +0300

    Add files via upload

---
+++ FinallProject/Models/Post.cs
@@ -0,0 +1,9 @@
+namespace blogger.Models {
+    public class Post {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Timestamp { get; set; }
+        public string Author { get; set; }
+    }
+}
